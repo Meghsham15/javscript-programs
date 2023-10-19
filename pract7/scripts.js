3374eb882d531e2669b2010dc042468f26286d2f
Meghsham <meghshamjade50@gmail.com>
2023-10-19 10:24:14 +0530

    7 pract update

--- pract7/scripts.js
+++ pract7/scripts.js
@@ -6,7 +6,20 @@ function insertObjToArray(arr,obj){
 
 let obj = {name:"Meghsham",roll:4215,year:"BE"};
 
+let arrayBefore = ['mj','insane',4215];
 let array = ['mj','insane',4215];
 
-let output = insertObjToArray(array,obj);
-console.log(output);
+// let output = insertObjToArray(array,obj);
+// console.log(output);
+
+let display = document.getElementById('display');
+let objInput = document.getElementById('obj');
+let insert = document.getElementById('insert');
+
+insert.addEventListener('click',(e)=>{
+    // console.log(objInput.value);
+    let objinp = objInput.value;
+    let output = insertObjToArray(array,objinp);
+    display.innerText = `array is - ${JSON.stringify(arrayBefore)} array after inserting user obj - ${JSON.stringify(output)}`;
+    // display.innerText = `hi`;
+});
